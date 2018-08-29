c8b44cee3d1609e1085e84d15ee8ee26cd36ccb8
codeprof <benutzer@desktop>
2018-08-29 19:02:08 +0200

    remove not needed sw

--- export-image/01-set-sources/01-run.sh
+++ export-image/01-set-sources/01-run.sh
@@ -8,5 +8,13 @@ apt purge dbus -y
 apt purge triggerhappy -y
 apt purge rfkill -y
 apt purge plymouth -y
+apt purge less -y
+apt purge ca-certificates -y
+apt purge perl -y
+apt purge alsa-utils -y #removes also raspi-config
+apt purge python* -y
+apt purge dosfstools -y
+apt purge logrotate -y
+apt purge mime-support -y
 apt autoremove -y
 EOF
